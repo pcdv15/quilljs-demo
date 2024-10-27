89eb386f0be6cdd351dc030a64190848dc55c122
Paul Chester <paulvillanueva2012@gmail.com>
2024-10-27 19:11:28 +0800

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
 import "./style.css";
 
 function App() {
   return (
     <>
-      <Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
+      >
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" sx={{ flexGrow: 1 }}>
@@ -12,6 +18,14 @@ function App() {
             </Typography>
           </Toolbar>
         </AppBar>
+        <Container
+          sx={{
+            minHeight: 0,
+            minWidth: "100vw",
+            flexGrow: 1,
+            backgroundColor: "#f1f6eb",
+          }}
+        ></Container>
       </Box>
     </>
   );
